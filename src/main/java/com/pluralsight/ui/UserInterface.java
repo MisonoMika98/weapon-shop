7a2fee75b9dbd7ced175d006b2158dac2d56b44f
ariel raymundo <ariraymundo98@gmail.com>
2026-05-22 10:09:29 -0700

    renamed class, working on UI

--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -1,4 +1,50 @@
 package com.pluralsight.ui;
 
-public class UserInterface {
+import java.util.Scanner;
+
+public class UserInterface
+{
+    private static Scanner userInput = new Scanner(System.in);
+
+
+    public static void homeScreen()
+    {
+        System.out.println();
+        System.out.println("Weapon Shop");
+        System.out.println("------------------------------------");
+        System.out.println("1) New Weapon Order");
+        System.out.println("0) Exit");
+        System.out.println();
+
+        while (true)
+        {
+            String choice = getUserInput("Select an option: ");
+
+            switch (choice)
+            {
+                case "1":
+                    // insert here
+                    break;
+
+                case "0":
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid option. Please try again.");
+                    System.out.println();
+            }
+        }
+    }
+
+
+
+
+
+
+    public static String getUserInput(String message)
+    {
+        System.out.print(message);
+        return userInput.nextLine().trim();
+    }
+
 }
--- src/main/java/com/pluralsight/util/ReceiptWriter.java
+++ src/main/java/com/pluralsight/util/ReceiptWriter.java
@@ -1,4 +1,4 @@
 package com.pluralsight.util;
 
-public class RecieptWriter {
+public class ReceiptWriter {
 }
