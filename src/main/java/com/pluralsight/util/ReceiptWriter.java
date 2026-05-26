d0ea68e2dd0f49effefeac7b216e58793a63c135
ariel raymundo <ariraymundo98@gmail.com>
2026-05-26 12:14:27 -0700

    fixed receiptwriter into correct formatting

--- src/main/java/com/pluralsight/util/ReceiptWriter.java
+++ src/main/java/com/pluralsight/util/ReceiptWriter.java
@@ -3,20 +3,29 @@ package com.pluralsight.util;
 import com.pluralsight.models.*;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ReceiptWriter
 {
     public static void writeOrder(Order order)
     {
-        try (FileOutputStream fileOutputStream = new FileOutputStream("data/orders.csv", true);
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+        String formattedDate = date.format(formatter);
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream("receipts/" + formattedDate + ".txt");
              PrintWriter printWriter = new PrintWriter(fileOutputStream))
         {
             for (OrderValuable item : order.getValuables())
             {
-                printWriter.print(item.getName() + "|$" + item.getPrice() + "|");
+                printWriter.println("Valuable:");
+                printWriter.println(item.getName() + " $" + item.getPrice());
+
             }
-            printWriter.print("$" + order.getTotal() + "|#" + order.getOrderNumber()); // outside the for loop so the order # doesn't repeat
-            printWriter.println(); // breaks onto next line inside .csv
+            printWriter.println("Order #: " + order.getOrderNumber());
+            printWriter.println("Order Date: " + formattedDate);
+            printWriter.println("Total Price: $" + order.getTotal());
 
         }
         catch(Exception ex)
